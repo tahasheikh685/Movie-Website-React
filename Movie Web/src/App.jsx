cf2702c33d05b4ed1f57f96860b73e8146f2cd5d
Intern3pk <Intern3pk@DESKTOP-QPK-01>
2023-11-07 17:42:09 +0500

    add search bar

--- Movie Web/src/App.jsx
+++ Movie Web/src/App.jsx
@@ -1,18 +1,49 @@
 
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react';
 
+const movie1= {
+  "Title": "Spiderman the Verse",
+  "Year": "2019–",
+  "imdbID": "tt12122034",
+  "Type": "series",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg"
+}
 
-function App() {
-  const [count, setCount]= useState(0);
+const API_URL= 'https://www.omdbapi.com?apikey=a9572c36'
+const App= () =>{
+
+  const searchMovies= async (title)=>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data= await response.json();
+
+    console.log(data.Search);
+  }
   
   useEffect(()=>{
-    alert('Reload');
-  });
+    searchMovies('spiderman')
+  }, [])
   return (
-    <div className='text-center bg-black text-white from-neutral-400'>
-      <button onClick={()=> setCount((prevCount)=> prevCount-1)}>-</button>
-      <h1>{count}</h1>
-      <button onClick={()=> setCount((nextCount)=> nextCount+1)}>+</button>
+    <div className='app'>
+        <h1>MovieLand</h1>
+        <div className='search'>
+          <input 
+          type="text"
+          placeholder='Search for Movies'
+          onChange={()=>{}} 
+          />
+          <img 
+          src="${searchIcon}" 
+          alt="search" 
+          onClick={()=>{}}
+          />
+        </div>
+        <div className='container'>
+          <div className='movie'>
+            <div>
+              <p>${movie1.Year}</p>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
